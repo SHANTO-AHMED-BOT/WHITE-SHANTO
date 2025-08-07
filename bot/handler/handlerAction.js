3e39fe9b22623be1598bf69ffcea7901c1ce79d8
SHANTO-AHMED-GOATBOT <cuteshanto455@gmail.com>
2025-08-07 11:11:15 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "🌸"){
-  if(event.userID == "61575889146877"){
+  if(event.userID == "61553937610145"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🐝"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61575889146877"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61553937610145"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
